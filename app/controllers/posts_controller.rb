a0cfe9eb2a98b32877c0d0be7eb013856067bebf
Your Actual Name <your actual email>
2016-07-02 16:03:58 +0200

    Intial Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+	before_action :find_post,only [:show, :edit, :ubdate]
+  def index
+  	@posts = Post.all.order ("created_at DESC")
+ end
+
+   def new 
+   	@post = Post.new
+   end
+
+   def create
+   	@post = Post .new (post_params)
+
+   	if @post.save
+   		redirect_to @post 
+   	else 
+   		render 'new'
+   	end 
+   end 
+   def show 
+   	@post = Post.find (params [:id])
+   end 
+   def edit 
+   	@post = Post.find(params[:id])
+   end 
+   def ubdate 
+   	@post = Post.find(params[:id])
+   	if @post = ubdate(post_params)
+   		redirect_to @post 
+   	else 
+   		render "edit"
+   	end
+   end 
+
+private 
+def post_params 
+	params.require(:post).permit (:title, :body )
+ end 
+end 
